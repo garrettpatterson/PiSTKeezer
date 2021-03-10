0056acc5e60860b1a655b2ad09d8f65dd379359b
Garrett Patterson <garrettpatterson@Garretts-MacBook-Pro.local>
2021-03-09 23:02:00 -0800

    Script to prototype ST calls and manage value changes

---
+++ st_keezer.py
@@ -0,0 +1,65 @@
+import requests
+import os
+import json
+import time
+
+conf = {
+	'app_url':"https://graph-na04-useast2.api.smartthings.com/api/smartapps/installations/"
+	, 'app_id':"c06ad6d1-1929-42b8-8736-a112301c9acd"
+	, 'token':"4ad77334-8ec6-4647-918e-df6cfceaa743"
+	, 'update_ignore_seconds':600
+	,'last_update':0
+	, 'last_event':{
+		'multiSensor':[0,0]
+		,'shanktemp':0
+		,'psi':0
+		,'door':'closed'
+	}
+}
+
+
+
+def update_event(sensor, val):
+	sensor_path = ['update']
+
+	last_val = conf['last_event'][sensor]
+	if last_val != val or time.time() - conf['last_update'] > conf['update_ignore_seconds']:
+		print("updated data")
+		conf['last_event'][sensor] = val
+		sensor_path.append(sensor)
+		if not isinstance(val,list):
+			val = [val]
+
+		sensor_path+=val
+		print(sensor_path)
+
+		update_path = '/'.join(map(str,sensor_path))
+
+		headers = {"Authorization": "Bearer "+conf['token']}
+
+		r = requests.put(conf['app_url'] + conf['app_id'] + '/' + update_path, headers=headers)
+
+		if r.status_code in [200,204]:
+			print("success")
+			conf['last_update'] = time.time()
+			with open('conf.json', 'w') as cfg:
+				cfg.write(json.dumps(conf))
+		else:
+			print(r.text)
+	else:
+		print("no value change since last update %d seconds ago" % (time.time()-conf['last_update']))
+
+
+
+if os.path.exists('conf.json'):
+	with open('conf.json','r') as cf:
+		conf = json.loads(cf.read())
+		print('load existing conf'+ json.dumps(conf))
+else:
+	print('create conf.json')
+	
+	with open('conf.json','w+') as cf:
+		cf.write(json.dumps(conf))
+
+
+update_event('multiSensor',[32,55])
